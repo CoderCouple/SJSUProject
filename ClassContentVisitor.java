1a4ca88dca2d9088c30c295688c81da5b14aa33c
Sunil Tiwari <sunil28071987@gmail.com>
2017-03-16 17:38:22 -0700

    Update ClassContentVisitor.java

--- ClassContentVisitor.java
+++ ClassContentVisitor.java
@@ -7,16 +7,6 @@ public class ClassContentVisitor extends VoidVisitorAdapter<Object> {
 	@Override
 	public void visit(ClassOrInterfaceDeclaration n, Object arg) {
 		super.visit(n, arg);
-		// System.out.println(n.getName());
-		//System.out.println(n.isInterface());
-//		String ClassType = null;
-//		if (n.isInterface()) {
-//			ClassType = "I";
-//			MainRun.MethodEndSpecifier=";";
-//		} else {
-//			ClassType = "C";
-//			MainRun.MethodEndSpecifier="";
-//		}
 		MainRun.ClassList.add(n.toString().substring(0, n.toString().indexOf("{")).replaceAll("public ", ""));
 		System.out.println(n.toString().substring(0, n.toString().indexOf("{")).replaceAll("public ", ""));
 		//String ClassDeclaration = n.toString().substring(0, n.toString().indexOf("{")).replaceAll("public ", "");
