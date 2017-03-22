dda860c43fc85f5adec33685900819bac970da3b
Sunil Tiwari <sunil28071987@gmail.com>
2017-03-21 18:19:30 -0700

    Update AttributeContentVisitor.java

--- AttributeContentVisitor.java
+++ AttributeContentVisitor.java
@@ -4,7 +4,6 @@ import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 	public class AttributeContentVisitor extends VoidVisitorAdapter<Void> {
-
 		@Override
 		public void visit(FieldDeclaration n, Void arg) {
 			super.visit(n, arg);
