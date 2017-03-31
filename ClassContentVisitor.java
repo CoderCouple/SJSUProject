70f1a6b685b5657776d956562a067c8616083d17
Sunil Tiwari <sunil28071987@gmail.com>
2017-03-31 11:35:22 -0700

    Update ClassContentVisitor.java

--- ClassContentVisitor.java
+++ ClassContentVisitor.java
@@ -4,6 +4,7 @@ import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 
+
 public class ClassContentVisitor extends VoidVisitorAdapter<Object> {
 	@Override
 	public void visit(ClassOrInterfaceDeclaration n, Object arg) {
