2ab4014cd2620dd5f1531c5a0470cb3c9cf13b87
Sunil Tiwari <sunil28071987@gmail.com>
2017-04-03 11:26:27 -0700

    Create ImportVisitor.java

---
+++ ImportVisitor.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+import com.github.javaparser.ast.ImportDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+public class ImportVisitor extends VoidVisitorAdapter<Object> {
+	@Override
+	public void visit(ImportDeclaration n, Object arg) {
+		super.visit(n, arg);
+		System.out.println(n.toString());
+		String FileContent=n.toString();
+		try {
+			FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS,FileContent);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
