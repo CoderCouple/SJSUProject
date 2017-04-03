7056144c197d044b9320fcc961237fd65239cc64
Sunil Tiwari <sunil28071987@gmail.com>
2017-04-03 11:20:41 -0700

    Update AttributeDependencyVisitor.java

--- AttributeDependencyVisitor.java
+++ AttributeDependencyVisitor.java
@@ -9,8 +9,11 @@ public class AttributeDependencyVisitor extends VoidVisitorAdapter<Void> {
 	public void visit(FieldDeclaration n, Void arg) {
 		super.visit(n, arg);
 		System.out.println(n.toString());
-	
+		//MainRun.AttributeList.add(n.toString());
 		String AttributeDeclaration = n.toString();
+		//System.out.println(
+		//		n.toString().substring(n.toString().substring(0, n.toString().lastIndexOf(" ")).lastIndexOf(" ") + 1,
+		//				n.toString().lastIndexOf(" ")));
 		String AttributeType = n.toString().substring(
 				n.toString().substring(0, n.toString().lastIndexOf(" ")).lastIndexOf(" ") + 1,
 				n.toString().lastIndexOf(" "));
@@ -22,7 +25,6 @@ public class AttributeDependencyVisitor extends VoidVisitorAdapter<Void> {
 				DependencyVisitor.getAttributeDependencyComment(AttributeType, AttributeDeclaration);
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
-				
 				e.printStackTrace();
 			}
 
