0617029aeab8e3644ec3f57e50e459f5c6b4b602
Sunil Tiwari <sunil28071987@gmail.com>
2017-04-03 11:23:54 -0700

    Update ClassDependencyVisitor.java

--- ClassDependencyVisitor.java
+++ ClassDependencyVisitor.java
@@ -1,115 +1,30 @@
 import java.io.FileNotFoundException;
-import java.io.IOException;
 
-public class DependencyVisitor {
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
-	public static void startDepencyComment() {
+public class ClassDependencyVisitor extends VoidVisitorAdapter<Object> {
+	@Override
+	public void visit(ClassOrInterfaceDeclaration n, Object arg) {
+		super.visit(n, arg);
+		// System.out.println(n.getName());
+		// System.out.println(n.isInterface());
+		String ClassType = null;
+		if (n.isInterface()) {
+			ClassType = "I";
+			MainRun.MethodEndSpecifier = ";";
+		} else {
+			ClassType = "C";
+			MainRun.MethodEndSpecifier = "";
+		}
+		//MainRun.ClassList.add(n.toString().substring(0, n.toString().indexOf("{")).replaceAll("public ", ""));
+		System.out.println(n.toString().substring(0, n.toString().indexOf("{")).replaceAll("public ", ""));
+		String ClassDeclaration = n.toString().substring(0, n.toString().indexOf("{")).replaceAll("public ", "");
 		try {
-			FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS, "/**");
-		} catch (IOException e) {
+			DependencyVisitor.getClassDependencyComment(ClassType, ClassDeclaration);
+		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-	}
-	public static void defaultDepencyComment() {
-		try {
-			FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS, " *@opt all");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	public static void endDepencyComment() {
-		try {
-			FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS, " */");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	public static void getClassDependencyComment(String ClassType, String ClassDeclaration) throws FileNotFoundException {
-
-		if (ClassDeclaration.contains("extends") && ClassDeclaration.contains("implements")) {
-			try {
-				FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS, "  * @opt inferrel");
-				FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS, "  * @opt inferdep");
-				FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS, "  * @opt inferdepinpackage/");
-				} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			System.out.println(" * @opt inferdepinpackage ");
-			System.out.println(" * @opt inferrel");
-			System.out.println(" * @opt inferdep ");
-			}else if (ClassDeclaration.contains("extends") && !ClassDeclaration.contains("implements")) {
-				try {
-					FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS, "  * @opt all ");
-					} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			System.out.println("/** @opt all */");
-		} else if (!ClassDeclaration.contains("extends") && ClassDeclaration.contains("implements")) {
-			try {
-				FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS, "  * @opt inferrel");
-				FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS, "  * @opt inferdep");
-				FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS, "  * @opt inferdepinpackage/");
-				} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("/**  * @opt inferrel");
-			System.out.println("* @opt inferdep ");
-			System.out.println("* @opt inferdepinpackage */");
-		}
-
-	}
-
-	public static void getMethodDependencyComment(String MethodParameter) throws FileNotFoundException {
-		for (int i = 0; i < MainRun.FileListAcess.size(); i++) {
-			if (MainRun.FileListAcess.get(i).equals(MethodParameter)) {
-				System.out.println("/** @depend - <uses> - " + MethodParameter + " */");
-				try {
-					FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS, "* @depend - <uses> - " + MethodParameter + "");
-					} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-
-		}
-
-	}
-
-	public static void getAttributeDependencyComment(String AttributeType, String AttributeDeclaration)
-			throws FileNotFoundException {
-
-		if (AttributeDeclaration.contains("<") || AttributeDeclaration.contains("[")) {
-			for (int i = 0; i < MainRun.FileListAcess.size(); i++) {
-				if (MainRun.FileListAcess.get(i).equals(AttributeType)) {
-					try {
-						FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS, "  * @composed 1 Has 1..* " + AttributeType + "");
-						} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-					System.out.println("/** @composed 1 Has 1..* " + AttributeType + " */");
-				}
-			}
-		}
-		for (int i = 0; i < MainRun.FileListAcess.size(); i++) {
-			if (MainRun.FileListAcess.get(i).equals(AttributeType)) {
-				try {
-					FileOperations.readWriteFileContent(MainRun.TARGET_JAVA_FILE_ACCESS, "  * @depend - <uses> - " + AttributeType+"");
-					} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				System.out.println("/** @depend - <uses> - " + AttributeType + " */");
-			}
 		}
 
 	}
